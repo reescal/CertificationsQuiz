d783f03edbfb16b00e52c690a2d3ed5935f9ae40
Roberto Escalera <roberto.escalera@headspring.com>
2022-02-13 15:25:10 -0600

    Fixed response open API messages to be more accurate. Can now delete quizzes.

--- src/CertificationsQuizAPI/quiz.cs
+++ src/CertificationsQuizAPI/quiz.cs
@@ -14,6 +14,8 @@ using System.IO;
 using Newtonsoft.Json;
 using System;
 using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Documents.Client;
+using System.Linq;
 
 namespace Headspring.CertificationsQuiz
 {
@@ -27,15 +29,15 @@ namespace Headspring.CertificationsQuiz
         {
             _logger = log;
             _mapper = mapper;
-            _container = cosmosClient.GetContainer("CertificationsQuiz", "Quiz");
+            _container = cosmosClient.GetContainer("CertificationsQuiz", "Items");
         }
 
         [FunctionName("quiz")]
         [OpenApiOperation(operationId: "GetQuizzes", tags: new[] { "Quiz" })]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(IEnumerable<Quiz>), Description = "The OK response")]
         public IActionResult GetQuizzes(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
-            [CosmosDB(databaseName: "CertificationsQuiz", collectionName: "Quiz", ConnectionStringSetting = "CosmosDBConnection", SqlQuery = "SELECT * FROM c WHERE c.Type = 'Quiz'")]IEnumerable<Quiz> quizzes)
+            [CosmosDB(databaseName: "CertificationsQuiz", collectionName: "Items", ConnectionStringSetting = "CosmosDBConnection", SqlQuery = "SELECT * FROM c WHERE c.Type = 'Quiz'")]IEnumerable<Quiz> quizzes)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
@@ -45,10 +47,10 @@ namespace Headspring.CertificationsQuiz
         [FunctionName("quizById")]
         [OpenApiOperation(operationId: "GetQuizById", tags: new[] { "Quiz" })]
         [OpenApiParameter(name: "id", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "The **Id** parameter")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(Quiz), Description = "The OK response")]
         public IActionResult GetQuizById(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "quiz/{id}")] HttpRequest req,
-            [CosmosDB(databaseName: "CertificationsQuiz", collectionName: "Quiz", ConnectionStringSetting = "CosmosDBConnection", Id = "{id}", PartitionKey = "{id}")]Quiz quizById,
+            [CosmosDB(databaseName: "CertificationsQuiz", collectionName: "Items", ConnectionStringSetting = "CosmosDBConnection", Id = "{id}", PartitionKey = "{id}")]Quiz quizById,
             string id)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
@@ -103,6 +105,27 @@ namespace Headspring.CertificationsQuiz
             }
 
             return new OkObjectResult(_mapper.Map<Quiz>(response.Resource));
+        }
+
+        [FunctionName("deleteQuiz")]
+        [OpenApiOperation(operationId: "DeleteQuiz", tags: new[] { "Quiz" })]
+        [OpenApiParameter(name: "id", In = ParameterLocation.Path, Required = true, Type = typeof(string), Description = "The **Id** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Description = "The OK response")]
+        public static async Task<IActionResult> DeleteQuiz(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "delete", Route = "quiz/{id}")]HttpRequest req,
+            [CosmosDB(ConnectionStringSetting = "CosmosDBConnection")] DocumentClient client,
+            string id)
+        {
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri("CertificationsQuiz", "Items");
+            var document = client.CreateDocumentQuery(collectionUri).Where(t => t.Id == id)
+                    .AsEnumerable().FirstOrDefault();
+
+            if (document == null)
+                return new NotFoundResult();
+                
+            await client.DeleteDocumentAsync(document.SelfLink, 
+                new Microsoft.Azure.Documents.Client.RequestOptions { PartitionKey = new Microsoft.Azure.Documents.PartitionKey(document.Id) });
+            return new OkResult();
         }
     }
 }
