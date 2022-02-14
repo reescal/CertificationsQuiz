da3b03f58561c020700c6a2c049757ad652305ff
Roberto Escalera <roberto.escalera@headspring.com>
2022-02-14 11:18:51 -0600

    TakeQuiz page takes in as parameters a list of ids (on url), gets list of question ids from azure functions, then displays those questions on the quiz. Quiz and choices are randomized.

---
+++ src/CertificationsQuiz/Features/TakeQuiz.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using CertificationsQuiz.Infrastructure;
+using CertificationsQuiz.Shared;
+using MediatR;
+
+namespace CertificationsQuiz.Features
+{
+    public class TakeQuiz
+    {
+        public class Query : IRequest<ViewModel>
+        {
+            public string Ids { get; set; }
+        }
+
+        public class ViewModel : IRequest
+        {
+            public IEnumerable<string> QuestionIds { get; set; }
+        }
+
+        public class QueryHandler : IRequestHandler<Query, ViewModel>
+        {
+            private readonly IQuizService _quizService;
+
+            public QueryHandler(IQuizService quizService)
+            {
+                _quizService = quizService;
+            }
+
+            public async Task<ViewModel> Handle(Query query, CancellationToken cancellationToken)
+            {
+                var ids = query.Ids.Split("&").AsEnumerable();
+                var questions = await _quizService.GetQuizzesQuestions(ids);
+                return new ViewModel { QuestionIds = questions };
+            }
+        }
+    }
+}
